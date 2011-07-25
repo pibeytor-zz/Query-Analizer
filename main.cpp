e95bc7c6546604c26779c1e78b629014dff97b6c
Ahmed Castro <antares_43@hotmail.com>
2011-07-24 20:18:37 -0600

    commit del proyecto

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: userr
+ *
+ * Created on July 24, 2011, 8:02 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    cout<<"Hola mundo!";
+    return 0;
+}
+
