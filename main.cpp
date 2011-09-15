57cd27442fe9deb2b767369138f57ada1cda43b4
Ahmed Castro <antares_43@hotmail.com>
2011-09-15 14:34:48 -0600

    pull

--- main.cpp
+++ main.cpp
@@ -17,6 +17,32 @@ int main()
 {
     StorageManager sm;
     sm.createTableSpace("MiBdd","1.0","clave",40);
+    InfoMDC imdc;
+    imdc.tipo_campo=1;
+    strcpy(imdc.nombre_campo,"campo");
+    imdc.size=sizeof(short);
+    imdc.nulls=false;
+    imdc.PK=false;
+    strcpy(imdc.DEFAULT,"mi default");
+
+    InfoMDC imdc2;
+    imdc2.tipo_campo=1;
+    strcpy(imdc2.nombre_campo,"campo");
+    imdc2.size=sizeof(short);
+    imdc2.nulls=false;
+    imdc2.PK=false;
+    strcpy(imdc2.DEFAULT,"mi default");
+
+    InfoMDC arr_imdc[2];
+    arr_imdc[0]=imdc;
+    arr_imdc[1]=imdc2;
+
+    sm.createTable("tabla",2,arr_imdc);
+
+    SystemBlock sb;
+    sb.escribir();
+    sb.acomodarPrimerLibre();
+    cout<<sb.getFree();
     /*
     for(;;)
     {
