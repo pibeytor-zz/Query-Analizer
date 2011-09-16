48fa1ae6cad8a07458d0d3dc88c393276d0aa2c4
pibeytor <pibeytor@hotmail.com>
2011-09-15 22:21:12 -0600

    los gets y sets

--- smm/smm.h
+++ smm/smm.h
@@ -3,39 +3,52 @@ class Field
 public:
 	string tipo;//"varchar" "booleano" "entero" "caracter"
 	string varchar;
-        bool booleano;
+    bool booleano;
 	int entero;
 	char caracter;
 
-        Field(string varchar)
+    Field(string varchar)
 	{
 		this->varchar=varchar;
 		tipo="varchar";
 	}
-        Field(bool booleano)
+    Field(bool booleano)
 	{
 		this->booleano=booleano;
 		tipo="booleano";
 	}
-        Field(int entero)
+    Field(int entero)
 	{
 		this->entero=entero;
 		tipo="entero";
 	}
-        Field(char caracter)
+    Field(char caracter)
 	{
-		this->caracter=caracter;
+                this->caracter=caracter;
 		tipo="caracter";
 	}
+    Field(){}
 };
 
 class Reccord
 {
-	vector<Field>fields;
+    public:
 
-        Reccord(vector<Field>fields);
-        Field getField(int id);
-        void setField(int id,Field field);
+        vector<Field>fields;
+
+        Reccord(vector<Field>fields){
+            this->fields=fields;
+        }
+
+        Field getField(int id)
+        {
+            return this->fields[id];
+        }
+
+        void setField(int id,Field field)
+        {
+            this->fields[id]=field;
+        }
 };
 
 class InfoField
@@ -46,7 +59,14 @@ class InfoField
 	bool acepta_null;
         Field valor_default;
 
-        InfoField(string tipo,string nombre,bool pk,bool acepta_null,Field valor_default);
+        InfoField(string tipo,string nombre,bool pk,bool acepta_null,Field valor_default)
+        {
+            this->tipo=tipo;
+            this->nombre=nombre;
+            this->pk=pk;
+            this->acepta_null=acepta_null;
+            this->valor_default=valor_default;
+        }
 };
 
 class Table
@@ -55,11 +75,28 @@ class Table
 	vector<InfoField>info_fields;
 	vector<Reccord>reccords;
 
-        Table(string nombre,vector<InfoField>info_fields);
-        void insertReccord(Reccord reccord);
-        void deleteReccord(int id);
-        void updateReccord(int id,Reccord reccord);
-        Reccord selectReccord(int id);
+        Table(string nombre,vector<InfoField>info_fields)
+        {
+            this->nombre=nombre;
+            this->info_fields=info_fields;
+        }
+
+        void insertReccord(Reccord reccord)
+        {
+            this->reccords.push_back(reccord);
+        }
+        void deleteReccord(int id)
+        {
+            this->reccords.erase(reccords.begin()+id);
+        }
+        void updateReccord(int id,Reccord reccord)
+        {
+            this->reccords[id]=reccord;
+        }
+        Reccord selectReccord(int id)
+        {
+            return this->reccords[id];
+        }
 };
 
 class StorageManagerM
